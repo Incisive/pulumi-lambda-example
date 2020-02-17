01ee493e598ebab8e744243afaf1e86234f9c40b
Ryan Smith <rsmith@redventures.com>
2020-02-17 15:03:10 -0500

    Updated

---
+++ infra/prod/domain.com/lambda/api/lib/jira.js
@@ -0,0 +1,6 @@
+
+let createJiraTicket = async function({ name, email, business_unit, additional_notes, project_name, previously_tested, exposure, compliance_driven, engagement_type }) {
+    return {};//dummy
+};
+
+exports.createJiraTicket = createJiraTicket;
